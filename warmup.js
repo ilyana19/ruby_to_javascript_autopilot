521eca0c0fb1c1075bab3a27ee1443ec708f389c
ilyana19 <aries_2152@hotmail.com>
2018-09-04 23:30:12 -0400

    finished warmup

---
+++ warmup.js
@@ -0,0 +1,56 @@
+var apples = 14;
+console.log(apples);
+console.log("I have " + apples + " apples.");
+
+var materials = ['wood', 'metal', 'stone'];
+var words = {
+  'elephant': "The world's largest land mammal.",
+  'school': "A place of learning.",
+  'ice cream': "A delicious milk-based dessert."
+}
+
+console.log(materials);
+console.log(words);
+
+var num = 16;
+if(num > 10) {
+  console.log(num + " is greater than 10");
+} else if(num == 10) {
+  console.log(num + " is exactly 10.");
+} else {
+  console.log(num + " must be less than 10.");
+}
+
+for(var i = 0; i < 10; i++) {
+  console.log("Doing the same thing over and over.");
+}
+
+var base = 5;
+for(var i = 1; i < 20; i++) {
+  num = i + base;
+  console.log(num);
+}
+
+var total = 0;
+for(var i = 0; i < 100; i++) {
+  total += i;
+}
+console.log(total);
+
+for(var i = 3; i < 15; i++) {
+  if(i > 9) {
+    console.log("You can get on the rollercoaster!");
+  } else {
+    console.log("You are too short to ride this rollercoaster.");
+  }
+}
+
+var containers = ['purse', 'wallet', 'backpack'];
+for(var i = 0; i < containers.length; i++) {
+  console.log(containers[i]);
+}
+
+function helloWorld() {
+  console.log("Hello World!");
+}
+helloWorld();
